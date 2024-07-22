35690b26a3a24e93bfaa56174584e690775ddc6a
zxllo12 <poapog123@naver.com>
2024-07-22 15:28:20 +0900

    과제 1
    과제내용 추가

--- Program.cs
+++ Program.cs
@@ -2,9 +2,52 @@
 {
     internal class Program
     {
+
+        //과제 1. Chracter 클래스 선언하기
+        #region
+        class Chracter
+        {
+            int level;
+            int HP;
+            float MoveSpeed;
+            int ATK;
+
+            void MoveFoward()
+            {
+
+            }
+
+            void MoveBack()
+            {
+
+            }
+
+            void MoveLeft()
+            {
+
+            }
+
+            void MoveRight()
+            {
+
+            }
+
+            void Attack()
+            {
+
+            }
+
+            void GetHit()
+            {
+
+            }
+
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("객처지향 과제1");
         }
     }
 }
